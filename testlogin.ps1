d3f2a4c43e42fe1bca84a7eb19d1527c2d154292
unknown <Coyote@target-windows.acme.corp>
2020-11-18 19:49:30 -0800

    Update URL

--- testlogin.ps1
+++ testlogin.ps1
@@ -15,9 +15,9 @@ limitations under the License.
 #>
 $global:headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $global:headers.Add("Content-Type", "application/json")
-$global:url = "https://comp01a.cyber-ark-demo.local/passwordvault/api"
+$global:url = "https://pvwa.acme.corp"
 
-$uri="$url/Auth/cyberark/Logon"
+$uri="$url/PasswordVault/api/Auth/cyberark/Logon"
 
 $body = @{
     username = "Administrator"
