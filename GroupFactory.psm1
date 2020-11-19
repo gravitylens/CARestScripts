df56ca99984689383a404e15c25448e007428be7
Mike <Mike@acme.corp>
2020-11-18 21:09:12 -0800

    Updated to acme.corp domain name

--- GroupFactory.psm1
+++ GroupFactory.psm1
@@ -14,11 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 #>
 Import-Module ActiveDirectory
-$ADCred = New-Object System.Management.Automation.PSCredential("Administrator",$(ConvertTo-SecureString "Cyberark1" -AsPlainText -Force))
+$ADCred = New-Object System.Management.Automation.PSCredential("ACME\Administrator",$(ConvertTo-SecureString "Cyberark1" -AsPlainText -Force))
 
 function New-CAADGroup {
     param (
         [string]$GroupName
     )
-    New-ADGroup -credential $adcred -server "dc01.cyber-ark-demo.local" -Name $groupname -SamAccountName $groupname -GroupCategory Security -GroupScope Global -DisplayName $groupname -Path "OU=Cyber-Ark Groups,DC=cyber-ark-demo,DC=local"
+    New-ADGroup -credential $adcred -server "dc01.acme.corp" -Name $groupname -SamAccountName $groupname -GroupCategory Security -GroupScope Global -DisplayName $groupname -Path "OU=Groups,OU=CyberArk,OU=Domain Users and Groups,DC=acme,DC=corp"
 }
