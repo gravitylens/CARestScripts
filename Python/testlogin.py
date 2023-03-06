47a55130e1abd5440c364dc60ff42afa003f7b14
Jason Niles <jason@gravitylens.net>
2023-03-06 09:53:54 -0800

    debug

--- Python/testlogin.py
+++ Python/testlogin.py
@@ -1,4 +1,4 @@
-<#
+"""
 Copyright [2020] [CyberArk Software, Inc.]
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,10 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-#>
+"""
+import json
+import requests
+
 headers = {
     'Content-Type': 'application/json',
 }
@@ -27,6 +30,6 @@ body = {
     "concurrentSession": "false"
 }
 
-response = requests.post(uri, headers=headers, json=body)
+response = requests.post(uri, headers=headers, json=body, verify=False)
 headers['Authorization'] = response.json()
 print(response.json())
