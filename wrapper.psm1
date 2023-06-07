631a2459300246152f8950c731a372b981a602cf
gravitylens <31743459+gravitylens@users.noreply.github.com>
2023-06-07 14:09:28 -0500

    Revert

--- wrapper.psm1
+++ wrapper.psm1
@@ -163,10 +163,12 @@ function New-CASafeMember{
     PROCESS{
         $uri="$url/PasswordVault/WebServices/PIMServices.svc/Safes/$SafeName/Members"
         $body = @{
+            "member"= @{
                 "MemberName"=$MemberName
                 "SearchIn"=$SearchIn
                 "MembershipExpirationDate"=$MembershipExpirationDate
                 "Permissions"=$Permissions  
+            }
         } | ConvertTo-Json -Depth 3
 
         Try{
