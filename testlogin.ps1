f89b4c2de9d92b87939bcda05292e3c46a2dbb69
gravitylens <31743459+gravitylens@users.noreply.github.com>
2024-04-16 13:01:57 -0500

    Update testlogin.ps1

--- testlogin.ps1
+++ testlogin.ps1
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 #>
 # Headers
-$headers = @{"content-type" = "application/json"}
+$global:headers = @{"content-type" = "application/json"}
 
 # URI
 $uri="https://pvwa.acme.corp/PasswordVault/api/Auth/cyberark/Logon"
